1260d123418727db38dc1bc17059f42628c2e70a
jas112 <jas.sandmaster@gmail.com>
2024-08-06 13:43:54 -0700

    + components

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>ST_RMX03 assets 5</h1>
+      <h1>ST_RMX03 + components</h1>
     </>
   )
 }
